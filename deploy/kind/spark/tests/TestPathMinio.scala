8d3849f31329e5d1071756a5fa28a4caae2342cf
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2025-10-10 11:38:29 -0300

    Create TestPathMinio.scala

---
+++ deploy/kind/spark/tests/TestPathMinio.scala
@@ -0,0 +1,25 @@
+import org.apache.iceberg.aws.s3.S3FileIO
+import java.nio.charset.StandardCharsets
+import java.io.OutputStream
+
+// Cria a instância do FileIO do Iceberg
+val io = new S3FileIO()
+
+// Inicializa com as credenciais do MinIO
+import scala.jdk.CollectionConverters._
+io.initialize(Map(
+  "s3.endpoint" -> "http://minio:9000",
+  "s3.access-key-id" -> "minio",
+  "s3.secret-access-key" -> "minio123"
+).asJava)
+
+// Caminho do "arquivo teste" no bucket
+val path = "s3://warehouse/test_iceberg.txt"
+
+// Escreve algo simples no MinIO
+val out: OutputStream = io.newOutputFile(path).create()
+out.write("Hello Iceberg + MinIO!".getBytes(StandardCharsets.UTF_8))
+out.close()
+
+// Verifique no MinIO se o arquivo foi criado
+println(s"Arquivo criado em: $path")
