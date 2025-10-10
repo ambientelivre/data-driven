3d671e8e1c47f1cce10354cf7c0a1e57e35099ae
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2025-10-10 11:48:22 -0300

    Update TestPathMinio.scala

--- deploy/kind/spark/tests/TestPathMinio.scala
+++ deploy/kind/spark/tests/TestPathMinio.scala
@@ -6,7 +6,8 @@ val io = new S3FileIO()
 io.initialize(Map(
   "s3.endpoint" -> "http://minio:9000",
   "s3.access-key-id" -> "admin",
-  "s3.secret-access-key" -> "password"
+  "s3.secret-access-key" -> "password",
+  "s3.region" -> "us-east-1"
 ).asJava)
 
 val out = io.newOutputFile("s3://warehouse/test_iceberg.txt").create()  // precisa criar com sucesso
