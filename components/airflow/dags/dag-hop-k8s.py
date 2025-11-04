970571d20f918cfe9838cfca88b45a2a8d58a66f
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2025-11-04 15:02:54 -0300

    Update dag-hop-k8s.py

--- components/airflow/dags/dag-hop-k8s.py
+++ components/airflow/dags/dag-hop-k8s.py
@@ -1,52 +1,43 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-import datetime
+from airflow.utils.dates import days_ago
+from datetime import timedelta
 
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime.datetime(2025, 1, 1),
-    'retries': 0,
-    'execution_timeout': datetime.timedelta(minutes=10),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
 }
 
 with DAG(
-    dag_id='hop_sample_pipeline',
+    dag_id="hop_sample_pipeline",
     default_args=default_args,
-    description='Executa um pipeline de exemplo do Apache Hop no Kubernetes.',
-    schedule=None,
+    description="Executa um sample do Apache Hop usando KubernetesPodOperator",
+    schedule_interval=None,
+    start_date=days_ago(1),
     catchup=False,
-    tags=['hop', 'kubernetes', 'sample'],
+    tags=["hop", "kubernetes", "samples"],
 ) as dag:
-    
+
     executar_hop_sample = KubernetesPodOperator(
-        task_id='executar_hop_sample',
-        namespace='default',
-        name='hop-sample-pod',
-        image='apache/hop:2.9.0',  # ou a versão que você usa
-
+        task_id="executar_hop_sample",
+        name="hop-sample-pod",
+        namespace="default",
+        image="apache/hop:2.9.0",
         env_vars={
-            'HOP_LOG_LEVEL': 'Basic',
-            'HOP_FILE_PATH': '${PROJECT_HOME}/pipelines/samples/pipeline-hello-world.hpl',
-            'HOP_PROJECT_FOLDER': '/files',
-            'HOP_PROJECT_NAME': 'samples',
-            'HOP_RUN_CONFIG': 'local'
+            "HOP_LOG_LEVEL": "Basic",
+            "HOP_FILE_PATH": "/opt/hop/samples/pipelines/01-basic/01-basic.hpl",
+            "HOP_PROJECT_FOLDER": "/opt/hop/samples",
+            "HOP_PROJECT_NAME": "samples",
+            "HOP_RUN_CONFIG": "local",
         },
-
-        # monta o volume com os samples do Hop (você pode alterar o path local)
-        volumes=[k8s.V1Volume(
-            name='hop-samples-volume',
-            host_path=k8s.V1HostPathVolumeSource(path='/opt/hop/samples')
-        )],
-        volume_mounts=[k8s.V1VolumeMount(
-            mount_path='/files',
-            name='hop-samples-volume'
-        )],
-
-        cmds=['/bin/sh', '-c'],
-        arguments=['/usr/local/bin/hop-run.sh'],
-
-        get_logs=True,
+        kubernetes_conn_id="kind",
         is_delete_operator_pod=True,
-        kubernetes_conn_id='kind',
-        startup_timeout_seconds=300
+        in_cluster=False,
+        do_xcom_push=False,
     )
+
+    executar_hop_sample
